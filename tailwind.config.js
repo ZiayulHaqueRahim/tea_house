ee154d9fc780e05b689e3aa670ac4e3c3c29bcd1
Zia <ziair3812@gmail.com>
2024-08-21 05:40:01 +0600

    added banner section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  Add the Tailwind directives to your CSS
+  Add the @tailwind directives for each of Tailwind’s layers to your main CSS file.
+  
+  src/input.c
